024ceeed62bdb3a37a2865ef2cdf4156a3a0fa21
pjl <821216100@qq.com>
2019-05-22 13:08:35 +0800

    finished

--- main/main.js
+++ main/main.js
@@ -1,4 +1,22 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(a) {
+    var array=new Array();
+    while(a>0){
+    	array.unshift(a%10);
+    	a=parseInt(a/10);
+    };
+    var ans=new Array();
+    for(var j=0;j<lcd.length;++j){
+    	for(var i=0;i<array.length;++i){
+        	ans.push(lcd[j][array[i]]);
+        	ans.push(" ");
+        }
+    	ans.pop();
+    	ans.push("\n");
+    };
+    return ans.join("");
 };
+var lcd=[
+	['._.','...','._.','._.','...','._.','._.','._.','._.','._.'],
+	['|.|','..|','._|','._|','|_|','|_.','|_.','..|','|_|','|_|'],
+	['|_|','..|','|_.','._|','..|','._|','|_|','..|','|_|','..|'],
+];
